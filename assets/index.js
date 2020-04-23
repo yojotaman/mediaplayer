018f42be87c7a61792f44064b6d2e9afe5c5b550
jonathan <josagal.ing@gmail.com>
2020-04-23 15:19:51 -0500

    ejercicio de repaso del concepto de this

--- assets/index.js
+++ assets/index.js
@@ -4,7 +4,9 @@ import AutoPlay from './plugins/AutoPlay.js'
 const video = document.querySelector('video'); // selecciona el elemento
 const player = new MediaPlayer({
     el: video,
-    plugins: [new AutoPlay()]
+    plugins: [
+        // new AutoPlay()
+    ]
 });
 
 const button = document.querySelector('button');
